30f6c64aa9a9e65d4cfdbfbba0ef992ab12dce03
Andrew Smithson <smithson@uk.ibm.com>
2016-03-29 23:08:22 +0100

    Correct indentation

--- Tests/ServiceTests.swift
+++ Tests/ServiceTests.swift
@@ -12,25 +12,25 @@ import XCTest
 @testable import zosconnectforswift
 
 class ServiceTests: XCTestCase {
-    
-    let zosConnect = ZosConnect(hostName: "http://zosconnectmock.mybluemix.net", port: 80)
-
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+  
+  let zosConnect = ZosConnect(hostName: "http://zosconnectmock.mybluemix.net", port: 80)
+  
+  override func setUp() {
+    super.setUp()
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+  }
+  
+  override func tearDown() {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    super.tearDown()
+  }
+  
+  func testGetStatus() {
+    zosConnect.getService("dateTimeService") { (service) in
+      service?.getStatus({ (status) in
+        XCTAssertEqual(status, ServiceStatus.STARTED)
+      })
     }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-
-    func testGetStatus() {
-        zosConnect.getService("dateTimeService") { (service) in
-            service?.getStatus({ (status) in
-                XCTAssertEqual(status, ServiceStatus.STARTED)
-            })
-        }
-    }
-
+  }
+  
 }
