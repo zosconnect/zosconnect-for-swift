587a1d9e8e8a75976b32fa0934b664d3f3ad0735
Andrew Smithson <smithson@uk.ibm.com>
2016-05-17 16:45:28 +0100

    Remove variable declarations

--- Package.swift
+++ Package.swift
@@ -1,13 +1,5 @@
 import PackageDescription
 
-#if os(Linux)
-   let swiftyJsonUrl = "https://github.com/IBM-Swift/SwiftyJSON.git"
-   let swiftyJsonVersion = 3
-#else
-   let swiftyJsonUrl = "https://github.com/SwiftyJSON/SwiftyJSON.git"
-   let swiftyJsonVersion = 2
-#endif
-
 let package = Package(
     name: "zosconnectforswift",
     targets: [],
