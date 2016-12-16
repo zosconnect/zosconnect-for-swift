9a83a193a4f4222ffe3f95a95ee2aac40f4ca4de
Andrew Smithson <smithson@uk.ibm.com>
2016-12-16 10:32:25 +0000

    Revert to IBM-Swift/SwiftyJSON for Linux support

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ let package = Package(
     name: "zosconnectforswift",
     targets: [],
     dependencies: [
-      .Package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", versions: Version(1,0,0)..<Version(3, .max, .max)),
+      .Package(url: "https://github.com/IBM-Swift/SwiftyJSON", versions: Version(1,0,0)..<Version(15, .max, .max)),
       .Package(url: "https://github.com/IBM-Swift/Kitura-net",
           majorVersion: 1, minor: 3),
     ]
